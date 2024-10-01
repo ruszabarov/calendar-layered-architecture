912f11fa0cd6f04371b7eff8cb1fd939e0904a32
kamsi10 <kamsieneh2@gmail.com>
2024-10-01 19:04:15 -0400

    Added server folder

--- client/src/App.js
+++ client/src/App.js
@@ -8,3 +8,11 @@ function App() {
 }
 
 export default App;
+// add this to the react app because the api requests go through express
+export const fetchData = async () => {
+  const response = await fetch('/api/endpoint'); // No need to include the domain
+  const data = await response.json();
+  return data;
+};
+
+// would also need to run the app and create a build directory
